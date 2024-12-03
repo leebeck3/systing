c182fb3e0461673a04c08d90e942d94f411d6656
Roger L. Beckermeyer III <beckerlee3@gmail.com>
2024-12-03 15:52:38 -0600

    systing: updated script

--- vmlinux_gen.sh
+++ vmlinux_gen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #prolly needs sudo
+#could also use the bpf packages for the .h files in src/bpf/*bpf.c
 apt install bpftool
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > src/bpf/vmlinux.h
