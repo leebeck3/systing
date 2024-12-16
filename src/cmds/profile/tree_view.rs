697ef32aec9aab3eae0d074f3dac30988b5f3daa
Josef Bacik <josef@toxicpanda.com>
2024-12-16 13:33:54 -0500

    systing: rework the TUI to load on demand
    For super large systems the tui bogs down when there's a lot of threads.
    Instead only populate the tasks when we specifically ask for them.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- src/cmds/profile/tree_view.rs
+++ src/cmds/profile/tree_view.rs
@@ -5,12 +5,12 @@ use tui_realm_treeview::{Node, Tree, TreeView, TREE_CMD_CLOSE, TREE_CMD_OPEN};
 use tuirealm::terminal::CrosstermTerminalAdapter;
 use tuirealm::{
     application::PollStrategy,
-    command::{Cmd, Direction},
+    command::{Cmd, CmdResult, Direction},
     event::{Event, Key, KeyEvent, KeyModifiers},
     props::{Alignment, BorderType, Borders, Color, Style},
     terminal::TerminalBridge,
-    Application, Component, EventListenerCfg, MockComponent, NoUserEvent, Sub, SubClause,
-    SubEventClause, Update,
+    Application, Component, EventListenerCfg, MockComponent, NoUserEvent, State, StateValue, Sub,
+    SubClause, SubEventClause, Update,
 };
 
 use crate::cmds::profile::process::{Process, ProcessStat, TotalProcessStat};
@@ -18,6 +18,7 @@ use crate::cmds::profile::process::{Process, ProcessStat, TotalProcessStat};
 #[derive(Debug, PartialEq)]
 pub enum Msg {
     AppClose,
+    OpenProcess(String),
     None,
 }
 
@@ -30,15 +31,17 @@ pub enum Id {
 #[allow(dead_code)]
 struct Model {
     app: Application<Id, Msg, NoUserEvent>,
+    process_vec: Vec<Process>,
+    tree: Tree<String>,
     redraw: bool,
     quit: bool,
     terminal: TerminalBridge<CrosstermTerminalAdapter>,
 }
 
-fn newkey(key: &mut u64) -> String {
+fn newkey(pid: u32, key: &mut u64) -> String {
     let k = *key;
     *key += 1;
-    k.to_string()
+    format!("{}_{}", pid, k)
 }
 
 impl Model {
@@ -48,14 +51,11 @@ impl Model {
         );
         let title = format!("Processes: {}", processes.len()).to_string();
         let mut scroll = processes.len();
+        let tree = Tree::new(Self::processes_to_nodes(&processes));
         scroll /= 4;
         app.mount(
             Id::ProcessTree,
-            Box::new(ProcessTree::new(
-                Tree::new(Self::processes_to_nodes(processes)),
-                title,
-                scroll,
-            )),
+            Box::new(ProcessTree::new(tree.clone(), None, title, scroll)),
             vec![],
         )
         .expect("Failed to mount process tree");
@@ -75,6 +75,8 @@ impl Model {
             .expect("Failed to set focus to ProcessTree");
         Model {
             app,
+            process_vec: processes,
+            tree,
             quit: false,
             redraw: true,
             terminal: TerminalBridge::init_crossterm().expect("Failed to init terminal"),
@@ -87,22 +89,20 @@ impl Model {
         });
     }
 
-    fn proccess_to_nodes(process: &Process, key: &mut u64) -> Node<String> {
-        let mut pnode = Node::new(
-            newkey(key),
-            format!("{} ({})", process.comm, process.pid).to_string(),
-        );
+    fn process_to_nodes(pnode: &mut Node<String>, process: &Process) {
+        let mut key: u64 = 0;
+        let pid = process.pid;
         pnode.add_child(Node::new(
-            newkey(key),
+            newkey(pid, &mut key),
             format!("Cgroup ID: {}", process.cgid).to_string(),
         ));
 
-        let mut pstat = Node::new(newkey(key), "Process Stats".to_string());
+        let mut pstat = Node::new(newkey(pid, &mut key), "Process Stats".to_string());
         for stat in ProcessStat::iter() {
-            let mut snode = Node::new(newkey(key), stat.to_string());
+            let mut snode = Node::new(newkey(pid, &mut key), stat.to_string());
             for r in process.runs.iter() {
                 snode.add_child(Node::new(
-                    newkey(key),
+                    newkey(pid, &mut key),
                     format!("{}: {}", r.start_time.format("%H:%M:%S"), r.stat_str(&stat)),
                 ));
             }
@@ -110,12 +110,12 @@ impl Model {
         }
         pnode.add_child(pstat);
 
-        let mut pstat = Node::new(newkey(key), "Total Stats".to_string());
+        let mut pstat = Node::new(newkey(pid, &mut key), "Total Stats".to_string());
         for stat in TotalProcessStat::iter() {
-            let mut snode = Node::new(newkey(key), stat.to_string());
+            let mut snode = Node::new(newkey(pid, &mut key), stat.to_string());
             for r in process.runs.iter() {
                 snode.add_child(Node::new(
-                    newkey(key),
+                    newkey(pid, &mut key),
                     format!(
                         "{}: {}",
                         r.start_time.format("%H:%M:%S"),
@@ -127,10 +127,24 @@ impl Model {
         }
         pnode.add_child(pstat);
 
-        let mut pevents = Node::new(newkey(key), "Preempt Events".to_string());
+        let mut threads_node = Node::new(
+            format!("threads_{}", process.pid),
+            format!("Threads: {}", process.threads.len()).to_string(),
+        );
+        for thread in process.threads.iter() {
+            threads_node.add_child(Node::new(
+                format!("thread_{}_{}", process.pid, thread.pid),
+                format!("{} ({})", thread.comm, thread.pid).to_string(),
+            ));
+        }
+        if threads_node.count() > 1 {
+            pnode.add_child(threads_node);
+        }
+
+        let mut pevents = Node::new(newkey(pid, &mut key), "Preempt Events".to_string());
         for r in process.runs.iter() {
             let mut run_pevents = Node::new(
-                newkey(key),
+                newkey(pid, &mut key),
                 format!(
                     "Run: {}, events {}",
                     r.start_time.format("%H:%M:%S"),
@@ -140,23 +154,23 @@ impl Model {
             );
             for pevent in r.preempt_events.iter() {
                 let mut pevent_node = Node::new(
-                    newkey(key),
+                    newkey(pid, &mut key),
                     format!("{}: {}", pevent.comm, pevent.count).to_string(),
                 );
                 pevent_node.add_child(Node::new(
-                    newkey(key),
+                    newkey(pid, &mut key),
                     format!("Pid: {}", pevent.preempt_pid).to_string(),
                 ));
                 pevent_node.add_child(Node::new(
-                    newkey(key),
+                    newkey(pid, &mut key),
                     format!("Tgid: {}", pevent.preempt_tgid).to_string(),
                 ));
                 pevent_node.add_child(Node::new(
-                    newkey(key),
+                    newkey(pid, &mut key),
                     format!("Cgid: {}", pevent.cgid).to_string(),
                 ));
                 pevent_node.add_child(Node::new(
-                    newkey(key),
+                    newkey(pid, &mut key),
                     format!("Count: {}", pevent.count).to_string(),
                 ));
                 run_pevents.add_child(pevent_node);
@@ -168,27 +182,45 @@ impl Model {
         if pevents.count() > 1 {
             pnode.add_child(pevents);
         }
-        pnode
     }
 
-    fn processes_to_nodes(processes: Vec<Process>) -> Node<String> {
-        let mut key: u64 = 0;
-        let mut node = Node::new(newkey(&mut key), "Processes".to_string());
+    fn processes_to_nodes(processes: &Vec<Process>) -> Node<String> {
+        let mut node = Node::new("processes".to_string(), "Processes".to_string());
         for process in processes.iter() {
-            let mut pnode = Self::proccess_to_nodes(process, &mut key);
-            let mut threads_node = Node::new(
-                newkey(&mut key),
-                format!("Threads: {}", process.threads.len()).to_string(),
-            );
-            for thread in process.threads.iter() {
-                threads_node.add_child(Self::proccess_to_nodes(thread, &mut key));
-            }
-            if threads_node.count() > 1 {
-                pnode.add_child(threads_node);
-            }
-            node.add_child(pnode);
+            node.add_child(Node::new(
+                format!("process_{}", process.pid),
+                format!("{} ({})", process.comm, process.pid).to_string(),
+            ));
         }
         node
+    }
+
+    fn reload_tree(&mut self) {
+        let current_node = match self.app.state(&Id::ProcessTree).ok().unwrap() {
+            State::One(StateValue::String(node)) => Some(node),
+            _ => None,
+        };
+        let title = format!("Processes: {}", self.process_vec.len()).to_string();
+        let mut scroll = self.process_vec.len();
+        scroll /= 4;
+        self.app
+            .umount(&Id::ProcessTree)
+            .expect("Failed to unmount process tree");
+        self.app
+            .mount(
+                Id::ProcessTree,
+                Box::new(ProcessTree::new(
+                    self.tree.clone(),
+                    current_node,
+                    title,
+                    scroll,
+                )),
+                vec![],
+            )
+            .expect("Failed to mount process tree");
+        self.app
+            .active(&Id::ProcessTree)
+            .expect("Failed to set focus to ProcessTree");
     }
 }
 
@@ -198,6 +230,35 @@ impl Update<Msg> for Model {
         match msg.unwrap_or(Msg::None) {
             Msg::AppClose => {
                 self.quit = true;
+                None
+            }
+            Msg::OpenProcess(key) => {
+                match key.split('_').collect::<Vec<&str>>().as_slice() {
+                    ["process", pid] => {
+                        let pid = pid.parse::<u32>().unwrap();
+                        if let Some(node) = self.tree.root_mut().query_mut(&key) {
+                            node.clear();
+                            if let Some(process) = self.process_vec.iter().find(|p| p.pid == pid) {
+                                Self::process_to_nodes(node, process);
+                            }
+                        }
+                        self.reload_tree();
+                    }
+                    ["thread", pid, tid] => {
+                        let pid = pid.parse::<u32>().unwrap();
+                        let tid = tid.parse::<u32>().unwrap();
+                        if let Some(node) = self.tree.root_mut().query_mut(&key) {
+                            node.clear();
+                            if let Some(thread) = self.process_vec.iter().find(|p| p.pid == pid) {
+                                if let Some(thread) = thread.threads.iter().find(|t| t.pid == tid) {
+                                    Self::process_to_nodes(node, thread)
+                                }
+                            }
+                        }
+                        self.reload_tree();
+                    }
+                    _ => {}
+                }
                 None
             }
             Msg::None => None,
@@ -211,8 +272,16 @@ pub struct ProcessTree {
 }
 
 impl ProcessTree {
-    pub fn new(tree: Tree<String>, title: String, scroll: usize) -> Self {
-        let focus = tree.root().id().to_string();
+    pub fn new(
+        tree: Tree<String>,
+        inital_node: Option<String>,
+        title: String,
+        scroll: usize,
+    ) -> Self {
+        let focus = match inital_node {
+            Some(node) if tree.root().query(&node).is_some() => node,
+            _ => tree.root().id().to_string(),
+        };
         ProcessTree {
             component: TreeView::default()
                 .foreground(Color::Reset)
@@ -223,6 +292,7 @@ impl ProcessTree {
                 )
                 .inactive(Style::default().fg(Color::Gray))
                 .indent_size(3)
+                .preserve_state(true)
                 .scroll_step(scroll)
                 .title(title, Alignment::Left)
                 .highlighted_color(Color::LightYellow)
@@ -235,7 +305,7 @@ impl ProcessTree {
 
 impl Component<Msg, NoUserEvent> for ProcessTree {
     fn on(&mut self, ev: Event<NoUserEvent>) -> Option<Msg> {
-        match ev {
+        let result = match ev {
             Event::Keyboard(KeyEvent {
                 code: Key::Left,
                 modifiers: KeyModifiers::NONE,
@@ -266,7 +336,10 @@ impl Component<Msg, NoUserEvent> for ProcessTree {
             }) => self.perform(Cmd::Submit),
             _ => return None,
         };
-        Some(Msg::None)
+        match result {
+            CmdResult::Submit(State::One(StateValue::String(node))) => Some(Msg::OpenProcess(node)),
+            _ => Some(Msg::None),
+        }
     }
 }
 
